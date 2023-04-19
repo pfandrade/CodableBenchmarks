fbd4bad5f653be7fd6376170cc2cc921ceda0426
Paulo Andrade <paulo@outercorner.com>
2023-04-19 22:13:32 +0100

    Added benchmark for CBORCoding

--- Tests/PerformanceTests.swift
+++ Tests/PerformanceTests.swift
@@ -1,5 +1,6 @@
 import XCTest
 import Foundation
+import CBORCoding
 @testable import Airport
 
 let count = 10000 // 1, 10, 100, 1000, or 10000
@@ -13,7 +14,7 @@ class PerformanceTests: XCTestCase {
         ]
     }
     
-    func testPerformanceCodable() {
+    func testCodableDecoding() {
         self.measure {
             let decoder = JSONDecoder()
             let airports = try! decoder.decode([Airport].self, from: data)
@@ -21,11 +22,82 @@ class PerformanceTests: XCTestCase {
         }
     }
     
-    func testPerformanceJSONSerialization() {        
+    func testCodableEncoding() throws {
+        let decoder = JSONDecoder()
+        let airports = try decoder.decode([Airport].self, from: data)
+        
+        self.measure {
+            let encoder = JSONEncoder()
+            let data = try? encoder.encode(airports)
+            XCTAssertNotNil(data)
+        }
+    }
+    
+    func testCodableSize() throws {
+        let decoder = JSONDecoder()
+        let airports = try decoder.decode([Airport].self, from: data)
+        let encoder = JSONEncoder()
+        let data = try encoder.encode(airports)
+        print(size: data.count, for: "Codable")
+    }
+    
+    func testJSONSerializationDecoding() {
         self.measure {
             let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [[String: Any]]
             let airports = json.map{ Airport(json: $0) }
             XCTAssertEqual(airports.count, count)
         }
     }
+    
+    func testJSONSerializationEncoding() {
+        let airports = try! JSONSerialization.jsonObject(with: data, options: []) as! [[String: Any]]
+        self.measure {
+            let data = try? JSONSerialization.data(withJSONObject: airports)
+            XCTAssertNotNil(data)
+        }
+    }
+    
+    func testJSONSerializationSize() throws {
+        let airports = try JSONSerialization.jsonObject(with: data, options: []) as! [[String: Any]]
+        let data = try JSONSerialization.data(withJSONObject: airports)
+        print(size: data.count, for: "JSONSerialization")
+    }
+    
+    
+    func testCBORDecoding() throws {
+        let decoder = JSONDecoder()
+        let airports = try decoder.decode([Airport].self, from: data)
+        let encoder = CBOREncoder()
+        let data = try encoder.encode(airports)
+        self.measure {
+            let decoder = CBORDecoder()
+            let airports = try! decoder.decode([Airport].self, from: data)
+            XCTAssertEqual(airports.count, count)
+        }
+    }
+    
+    func testCBOREncoding() throws {
+        let decoder = JSONDecoder()
+        let airports = try decoder.decode([Airport].self, from: data)
+        
+        self.measure {
+            let encoder = CBOREncoder()
+            let data = try? encoder.encode(airports)
+            XCTAssertNotNil(data)
+        }
+    }
+    
+    func testCBORSize() throws {
+        let decoder = JSONDecoder()
+        let airports = try decoder.decode([Airport].self, from: data)
+        let encoder = CBOREncoder()
+        let data = try encoder.encode(airports)
+        print(size: data.count, for: "Codable")
+    }
+    
+    private func print(size: Int, for encoderName: String) {
+        Swift.print("\(encoderName) size: \(size) bytes (\(ByteCountFormatter.string(fromByteCount: Int64(size), countStyle: .file)))")
+    }
+    
+    
 }
